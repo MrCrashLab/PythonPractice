5a1cedbfb1a9f9900fa6b98c916851c5070af122
MrCrashLab <m.dudarev2002@mail.ru>
2021-02-15 22:45:32 +0300

    Commit

--- Practice_01/practice_01.py
+++ Practice_01/practice_01.py
@@ -1,26 +1,26 @@
 from math import log, sin, tan, exp
 
 
-def func1(x, y, z):
+def f11(x, y, z):
     return (53 * z ** 2 - sin(z) - 77) / (90 * z ** 8 - 17 * z ** 5 - 89) + \
            log(40 * z) + y ** 3 - \
            (sin(z) + 56 * z ** 7 + 64) / (sin(x) + 42 * x ** 8 + 38)
 
 
-def func2(x):
+def f12(x):
     if x < 82:
         return 53 * x ** 2 - sin(x) - 77
     elif 82 <= x < 148:
         return 90 * x ** 8 - 17 * x ** 5 - 89
     elif 148 <= x < 218:
-        return tan(x ** 2) + x ** 532
+        return tan(x ** 2) + x ** 5
     elif 218 <= x < 292:
         return 4 * x ** 6 - 66 * x ** 5
     elif x >= 292:
         return exp(x ** 3 - sin(x)) - (x ** 7) / 26
 
 
-def func3(n, m):
+def f13(n, m):
     res1 = 0
     res2 = 0
     for i in range(1, n + 1):
@@ -30,7 +30,7 @@ def func3(n, m):
     return 56 * res1 - 7 * res2
 
 
-def func4(n):
+def f14(n):
     if n == 0:
         return 4
     elif n == 1:
@@ -38,10 +38,4 @@ def func4(n):
     elif n < 0:
         return None
     else:
-        return 0.04 * func4(n - 1) - tan(func4(n - 1))
-
-
-print('%.2e' % func1(int(input('Введите x: ')), int(input('Введите y: ')), int(input('Введите z: '))))
-print('%.2e' % func2(int(input('Введите x: '))))
-print('%.2e' % func3(int(input('Введите n: ')), int(input('Введите m: '))))
-print('%.2e' % func4(int(input('Введите n: '))))
+        return 0.04 * f14(n - 1) - tan(f14(n - 1))
---
+++ Practice_02/practice_02.py
@@ -0,0 +1,45 @@
+def f21(x):
+    if x[2] == 1994:
+        if x[1] == 1981:
+            if x[4] == 'plsql':
+                if x[0] == 'nesc':
+                    return 0
+                elif x[0] == 'nsis':
+                    return 1
+                elif x[0] == 'oz':
+                    return 2
+            elif x[4] == 'xml':
+                if x[3] == 1963:
+                    return 3
+                elif x[3] == 2017:
+                    return 4
+                elif x[3] == 1962:
+                    return 5
+        elif x[1] == 1985:
+            return 6
+    elif x[2] == 2004:
+        if x[1] == 1981:
+            if x[0] == 'nesc':
+                if x[4] == 'plsql':
+                    return 7
+                elif x[4] == 'xml':
+                    return 8
+            elif x[0] == 'nsis':
+                if x[4] == 'plsql':
+                    return 9
+                elif x[4] == 'xml':
+                    return 10
+            elif x[0] == 'oz':
+                return 11
+        elif x[1] == 1985:
+            return 12
+    elif x[2] == 2006:
+        return 13
+
+
+def f22(x):
+    str_bin = str(bin(x))[2:]
+    a_str_bin = str_bin[-15:]
+    b_str_bin = str_bin[-29:-15]
+    c_str_bin = str_bin[-32:-29]
+    return hex(int(a_str_bin + c_str_bin + b_str_bin, 2))
