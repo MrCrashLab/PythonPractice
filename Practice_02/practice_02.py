75ed3604071c9d9d54e9a0637adb1fd308329b31
MrCrashLab <m.dudarev2002@mail.ru>
2021-02-18 00:43:02 +0300

    Commit

--- Practice_02/practice_02.py
+++ Practice_02/practice_02.py
@@ -44,4 +44,29 @@ def f22(x):
     a_str_bin = str_bin[-15:]
     b_str_bin = str_bin[-29:-15]
     c_str_bin = str_bin[:-29]
-    return int(a_str_bin + c_str_bin + b_str_bin, 2)
+    str_hex = str(hex(int(a_str_bin + c_str_bin + b_str_bin, 2)))
+    if len(str_hex) < 10:
+        return str(str_hex[:2] + '0' * (10 - len(str_hex)) + str_hex[2:])
+    return hex(int(a_str_bin + c_str_bin + b_str_bin, 2))
+
+
+def f23(x):
+    res = []
+    tmparr = []
+    for i in range(len(x)):
+        while None in x[i]:
+            x[i].remove(None)
+        tmparr.append(list())
+        for tmp in x[i]:
+            if tmp not in tmparr[i]:
+                tmparr[i].append(tmp)
+        res.append(list())
+        res[i].append(int(tmparr[i][0].split(':')[1][:-1]) / 100)
+        res[i].append(tmparr[i][0].split(':')[0][5:])
+        res[i].append(tmparr[i][1].replace('-', '.'))
+        res[i].append(tmparr[i][2].split(' ')[1].split('.')[0]+'. '+tmparr[i][2].split(' ')[0])
+    return res
+
+
+print(f23([[None, None, "+74327612358:99%", "17-03-01", "Шуфский Н.М.", "Шуфский Н.М."],
+           [None, None, "+70939609830:1%", "07-10-01", "Кесушяк Л.С.", "Кесушяк Л.С."]]))
