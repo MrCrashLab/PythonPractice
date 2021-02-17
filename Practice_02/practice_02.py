a34556cdea598e61170f9785d0f73451e5234f82
MrCrashLab <m.dudarev2002@mail.ru>
2021-02-17 18:36:46 +0300

    Commit

--- Practice_02/practice_02.py
+++ Practice_02/practice_02.py
@@ -39,10 +39,9 @@ def f21(x):
 
 def f22(x):
     str_bin = str(bin(x))[2:]
+    if len(str_bin) < 32:
+        str_bin = '0' * (len(str_bin) - 32) + str_bin
     a_str_bin = str_bin[-15:]
     b_str_bin = str_bin[-29:-15]
     c_str_bin = str_bin[:-29]
-    hex_str_bin = str(hex(int(a_str_bin + c_str_bin + b_str_bin, 2)))
-    if len(hex_str_bin) < 10:
-        return hex_str_bin[0:2] + '0' * (10 - len(hex_str_bin)) + hex_str_bin[2:]
-    return hex_str_bin
+    return int(a_str_bin + c_str_bin + b_str_bin, 2)
